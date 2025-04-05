d567318711c61806383bc17d79b6804fd1e19c73
meepmeep <elijahsabay@gmail.com>
2025-04-05 17:40:28 +0800

    added backend

---
+++ app/api/admin/appointment/route.jsx
@@ -0,0 +1,24 @@
+// import { createClient } from "@/utils/supabase/server";
+import { createClient } from '@supabase/supabase-js'; 
+
+import { NextResponse } from "next/server";
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+
+  export async function GET() {
+    const {data, error} = await supabase.from('BarangayAppointment').select('*')
+    console.log(data);
+
+    if (error) {
+        return new Response(JSON.stringify({ error: error.message }),{ status: 500 });
+    }
+    
+    return new Response(JSON.stringify(data),{ status: 200 });
+    // return NextResponse.json({
+    //     Hello:"world"
+    // });
+ 
+}
+

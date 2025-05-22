b8f28b1d918d849fde7cb1d44c8146c5aae8cba6
CodexPremiera <ashleyken.professional@gmail.com>
2025-05-22 16:47:10 +0800

    add dashboard page

--- components/citizen_desk/tabs/Dashboard.tsx
+++ components/citizen_desk/tabs/Dashboard.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import React, {useMemo, useState} from "react";
+import {AgePopulation} from "@/components/ui/charts/AgePopulation";
+import {SectorPopulation} from "@/components/ui/charts/SectorPopulation";
 
 
 const Dashboard = ()=> {
 	return (
-		<div className="flex flex-col gap-8 p-4 sm:p-6 background-1 rounded-[10px]">
-			Dashboard
+		<div className="flex flex-col lg:flex-row gap-8 p-4 sm:p-6 background-1 rounded-[10px]">
+			<div className="lg:w-[50%]">
+				<AgePopulation/>
+			</div>
+
+			<div className="lg:w-[50%]">
+				<SectorPopulation/>
+			</div>
 		</div>
 	);
 };

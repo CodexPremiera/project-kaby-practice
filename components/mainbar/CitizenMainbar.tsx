4a7beaaeae4e29aa8fca83598fee7f32ba4de665
CodexPremiera <ashleyken.professional@gmail.com>
2025-05-24 15:01:43 +0800

    add sidebar item highlight if page is active

--- components/mainbar/CitizenMainbar.tsx
+++ components/mainbar/CitizenMainbar.tsx
@@ -7,6 +7,7 @@ import {
 	RiSettings4Line,
 } from "react-icons/ri";
 import MainbarItem from "@/components/mainbar/MainbarItem";
+import {usePathname} from "next/navigation";
 
 const nav_items = [
 	{ name: "Home", path: `/home`, icon: RiCommunityLine },
@@ -15,12 +16,16 @@ const nav_items = [
 	{ name: "Settings", path: `/account`, icon: RiSettings4Line },
 ];
 
-const CitizenMainbar = () => (
-	<div className="mainbar">
-		{nav_items.map((item) => (
-			<MainbarItem key={item.path} {...item} />
-		))}
-	</div>
-);
+const CitizenMainbar = () => {
+	const pathname = usePathname();
+
+	return (
+		<div className="mainbar">
+			{nav_items.map((item) => (
+				<MainbarItem key={item.path} {...item} pathname={pathname} />
+			))}
+		</div>
+	)
+};
 
 export default CitizenMainbar;
